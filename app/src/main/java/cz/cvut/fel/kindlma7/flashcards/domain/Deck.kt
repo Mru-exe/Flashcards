ca5d86da4a6c87c11d743b4523d7b6f611c8aa07
mru-exe <martin@kindl.cz>
2026-04-22 17:17:15 +0200

    Added derived attributes to Deck (Model)

--- app/src/main/java/cz/cvut/fel/kindlma7/flashcards/domain/Deck.kt
+++ app/src/main/java/cz/cvut/fel/kindlma7/flashcards/domain/Deck.kt
@@ -3,5 +3,7 @@ package cz.cvut.fel.kindlma7.flashcards.domain
 data class Deck(
     val id: Long = 0,
     val name: String,
-    val topicId: Int? = null
+    val topicId: Int? = null,
+    val cardCount: Int,
+    val dueCount: Int
 )
